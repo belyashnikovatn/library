caf2be7effd0c38682aba89669f88272f62e4a2b
belyashnikovatn <101242667+belyashnikovatn@users.noreply.github.com>
2024-11-21 21:34:32 +0500

    Add List function

--- models.py
+++ models.py
@@ -10,20 +10,37 @@ class BookStatus(Enum):
     ABSENT = 'выдана'
 
 
-class Book:
+class Book():
     """Book class."""
+
+    library: list[object] = []
 
     def __init__(
             self, id: int, title: str, author: str, year: int,
             status: BookStatus = BookStatus.IN_STOCK
     ) -> None:
-        """Initialize an instance of a book."""
+        """Initialize a book and add into library."""
         self.id = id
         self.title = title
         self.author = author
         self.year = year
         self.status = status
 
+        Book.library.append(self)
+
+    @classmethod
+    def get_all(cls):
+        """Return a list of all books."""
+        return [book for book in Book.library]
+
+    def __str__(self):
+        """Return full description of a book."""
+        return (
+            f'Книга номер {self.id}: '
+            f'под названием "{self.title}" авторства {self.author}, '
+            f'{self.year} года выпуска сейчас {self.status.value}')
+
 
 book = Book(1, 'Сказки', 'Гоголь', 1952)
-print(f'Книга номер {book.id} = {book.title, book.author, book.year, book.status.value}')
+book = Book(2, 'Роман', 'Толстой', 1934)
+[print(book) for book in Book.get_all()]
