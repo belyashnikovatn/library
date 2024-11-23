de9399c0fd819993cb9174b78de3f05729252bf1
belyashnikovatn <101242667+belyashnikovatn@users.noreply.github.com>
2024-11-23 12:28:12 +0500

    Add Change status func

--- models.py
+++ models.py
@@ -19,7 +19,7 @@ class Book:
 
     def __init__(
             self, title: str, author: str, year: int,
-            id: int = None,
+            id: int = 0,
             status: BookStatus = BookStatus.IN_STOCK
     ) -> None:
         """Initialize a book and add into the library."""
@@ -36,14 +36,14 @@ class Book:
 
     @classmethod
     def _get_next_id(cls) -> int:
-        """Internal method: find max id and return next."""
+        """Find max id and return next."""
         if not Book.library:
             return 1
         return max([book.id for book in Book.library]) + 1
 
     @classmethod
     def _get_by_id(cls, id) -> Union[object, None]:
-        """Internal method: find a book by id."""
+        """Find a book by id."""
         if result := [book for book in Book.library if book.id == id]:
             return result[0]
         return None
@@ -65,6 +65,24 @@ class Book:
             print(f'Книга под номером {id} удалена.')
         else:
             print(f'Нет книги под номером {id}.')
+
+    @classmethod
+    def change_status(cls, id: int, status: str) -> None:
+        """Change a book status."""
+        book = Book._get_by_id(id)
+        if not book:
+            print(f'Нет книги под номером {id}.')
+            return None
+        if not status.lower() in [item.value.lower() for item in BookStatus]:
+            print(f'Статуса "{status}" нет.')
+            return None
+        if status.lower() == book.status.value.lower():
+            print(f'У книги под номером {id} уже статус "{status}"')
+            return None
+        new_status = [item for item in BookStatus if item.value.lower() == status.lower()][0]
+        book.status = new_status
+        print(f'Книга под номером {id} теперь "{book.status.value}"')
+
 
     @classmethod
     def get_all_by_param(
@@ -91,33 +109,37 @@ class Book:
             f'{self.year} года выпуска сейчас {self.status.value}')
 
 
-Book.get_all()
-Book('Сказки', 'Гоголь', 1952)
-Book('Роман', 'Толстой', 1934)
-Book('Стихи', 'Пушкин', 1934)
+# Book.get_all()
+# Book('Сказки', 'Гоголь', 1952)
+# Book('Роман', 'Толстой', 1934)
+# Book('Стихи', 'Пушкин', 1934)
 
-Book.get_all()
-Book.delete(2)
-Book.get_all()
-Book.delete(6)
+# Book.get_all()
+# Book.delete(2)
+# Book.get_all()
+# Book.delete(6)
 
-Book('Поэма', 'Блок', 1917)
+# Book('Поэма', 'Блок', 1917)
 
-Book.get_all()
-print('-------------')
-Book.get_all_by_param(author='Гоголь')
-Book.get_all_by_param(author='Г1231оголь')
+# Book.get_all()
+# print('-------------')
+# Book.get_all_by_param(author='Гоголь')
+# Book.get_all_by_param(author='Г1231оголь')
 
 test_book = Book('SinnSongs', 'People', 1212)
+Book.change_status(1, 'ДРУГОЙ')
+Book.change_status(1, 'в наличии')
+Book.change_status(1, 'ВЫДАНа')
+Book.get_all()
+# print([item.value for item in BookStatus])
+# print([item.name for item in BookStatus])
+# def some_function(value: str):
+#     try:
+#         value_atr = getattr(test_book, value)
+#         print (value_atr)
+#     except AttributeError as e:
+#         print(f'This is {e} error')
 
 
-def some_function(value: str):
-    try:
-        value_atr = getattr(test_book, value)
-        print (value_atr)
-    except AttributeError as e:
-        print(f'This is {e} error')
-
-
-some_function("title")
-some_function("title123")
+# some_function("title")
+# some_function("title123")
